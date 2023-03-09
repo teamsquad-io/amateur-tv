eaeb255b012be5fecd2872be1eaf6c950b4a9aca
Javier Casares <javier@casares.org>
2023-03-09 08:28:32 +0100

    security fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,7 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0757b69a27e9f0464d48');
+<?php
+// Make sure the file is not directly accessible.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'We\'re sorry, but you cannot directly access this file.' );
+}
+
+return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0757b69a27e9f0464d48');
